cc26a8547547500e3556f64378fd2c7b22f94e6c
Qu Xiaofeng <quxiaofeng.at.polyu@gmail.com>
2014-03-05 12:26:50 +0800

    refactor the CheckDebugStatus
    Signed-off-by: Qu Xiaofeng <quxiaofeng.at.polyu@gmail.com>

--- DebugStatus/CheckDebugStatus.m
+++ DebugStatus/CheckDebugStatus.m
@@ -1,46 +1,70 @@
-function [ debug ] = CheckDebugStatus( debug )
+function [ debug_state ] = CheckDebugStatus( debug_state )
 %CHECKDEBUGSTATUS Check debug status
-% debug = CheckDebugStatus(debug) translate a general debug description to
-% a standard logical value.
-% 
-% It returns true if input is
-%   1, true, 'debug', 'test'.
-% It returns false if input is
-%   0, false, 'release', 'run'.
-% Default and blank input leads to false.
+%    debug_state = CheckDebugStatus(debug_state) parse a general debug
+%    description to a standard logical value.
 %
-% After this, debug code can be simply surrounded by
-%   if debug
-%       ...
-%   end
+%
+%    It returns true (in a debugging state) if input is
+%       1, true, 'debug', 'test'.
+%    It returns false (in a non-debugging state) if input is
+%       0, false, 'release', 'run'.
+%    Default (blank) input leads to false. (Not debugging)
+%
+%
+%    After using this function, debug related code in scripts and functions
+%    can be simply surrounded by
+%
+%        if debug_state
+%            ...
+%        end
+%
+%
+%    In addition, the function using CheckDebugStatus can be formatted as
+%
+%       function [ result ] = Test(vars, debug)
+% 
+%           debug = CheckDebugStatus( debug );
+% 
+%           if debug
+%               display 'debugging';
+%               result = 'debugging';
+%           else
+%               result = 'running';
+%           end
+%           display(result);
+%       end
+%
+%    For more information, see <a href="matlab:
+%    web('https://github.com/quxiaofeng/balance')">balance</a>.
 
-% debug
+
 if nargin == 1
-    switch debug
+    switch debug_state
         case 1
-            debug = true;
+            debug_state = true;
         case 0
-            debug = false;
+            debug_state = false;
         case true
-            debug = true;
+            debug_state = true;
         case false
-            debug = false;
+            debug_state = false;
         case 'debug'
-            debug = true;
+            debug_state = true;
         case 'release'
-            debug = false;
+            debug_state = false;
         case 'test'
-            debug = true;
+            debug_state = true;
         case 'run'
-            debug = false;
+            debug_state = false;
         otherwise
-            error(['wrong debugflag(' ...
+            display(['wrong debugflag(' ...
                 '1, true,  ''debug'',   ''test'', ' ...
                 '0, false, ''release'', ''run''   ',...
                 ')']);
+            debug_state = false;
     end
 else
-    debug = false;
+    debug_state = false;
 end
 
 end
